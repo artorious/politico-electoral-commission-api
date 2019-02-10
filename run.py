634733e70156fad180003c7b5ab7018ee4bdebf7
Arthur Ngondo <ngondoarthur@gmail.com>
2019-02-10 13:25:29 +0300

    [Chore #163682717] edit init file

--- run.py
+++ run.py
@@ -3,8 +3,8 @@
 import os
 from app import create_app
 
-config_mode = os.getenv("APP_SETTINGS")
-app = create_app(config_mode)
+
+app = create_app("development")
 
 if __name__ == "__main__":
     app.run()
