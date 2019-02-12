141681276876ed44cbb6e841035bdc66f3cd73a2
Arthur Ngondo <ngondoarthur@gmail.com>
2019-02-12 06:54:30 +0300

    [chore #163801300] Update error messages
    Change hanldlng of out-of-range entities

--- app/api/v1/views/response_vars.py
+++ app/api/v1/views/response_vars.py
@@ -2,39 +2,39 @@
 """ Validates user input to interact with the API. """
 
 more_data_fields_response = {
-                "status": 400,
-                "error": "Bad Query - More data fields than expected"
-            }
+    "status": 400,
+    "error": "Bad Query - More data fields than expected"
+}
 
 few_data_fields_response = {
-                "status": 400,
-                "error": "Bad Query - Fewer data fields than expected"
-            }
+    "status": 400,
+    "error": "Bad Query - Fewer data fields than expected"
+}
 
 id_cannot_be_zero_response = {
-                "status": 400,
-                "error": "ID cannot be zero or negative"
-            }
+    "status": 400,
+    "error": "ID cannot be zero or negative"
+}
 
 entity_already_exists_response = {
-                "status": 409,
-                "error": "Conflict - Entity already exists"
-            }
+    "status": 409,
+    "error": "Conflict - Entity already exists"
+}
 
 id_out_of_range_response = {
-                    "status": 416,
-                    "error": "ID out of range. Requested Range Not Satisfiable"
-                }
+    "status": 416,
+    "error": "Entity not in server. ID out of range."
+}
 
 unprocessable_data_response = {
-                "status": 422,
-                "error": "Unprocessable Entity - Invalid value in data field"
-            }
+    "status": 422,
+    "error": "Unprocessable Entity - Invalid value in data field"
+}
 
 empty_data_field_response = {
-                "status": 422,
-                "error": "Empty data field"
-            }
+    "status": 422,
+    "error": "Empty data field"
+}
 
 expected_offices = ["Federal", "Legislative", "State", "Local Government"]
 
@@ -42,7 +42,4 @@ expected_party_fields = ["name", "hqAddress", "logoUrl", "Party members"]
 
 expected_office_fields = ["name", "type"]
 
-class Validate:
-    def __init__(self):
-        pass
 
--- app/tests/v1/test_views/test_office_views.py
+++ app/tests/v1/test_views/test_office_views.py
@@ -195,7 +195,7 @@ class TestFetchingOffice(TestOfficeRoutes):
         )
         self.assertEqual(
             deserialized_response["error"],
-            "ID out of range. Requested Range Not Satisfiable",
+            "Entity not in server. ID out of range.",
             msg="Response Body Contents- Should be custom message "
         )
 
