fe075ff27d197a2e5a92c136249679efa1c26615
Arthur Ngondo <ngondoarthur@gmail.com>
2019-02-19 21:16:31 +0300

    [Feature #163954137] Register aspiring candidates
    Register a user as a candidate running or a political office

---
+++ app/api/v2/views/candidate_views.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+""" Routes Candidate Information """
+
+from flask import Blueprint, jsonify, request
+from app.api.v2.models.candidates_models import Candidates
+from app.api.v2.models.database_models import DatabaseManager
+from app.api.v2.models.validation_helper import ValidationHelper
+
+BASE_BP_V2 = Blueprint("v2_base", __name__, url_prefix="/api/v2")
+
+
+@BASE_BP_V2.route("/office/<int:oid>/register", methods=["POST"])
+def create_candidate(oid):
+    """ Create a candidate """
+    custom_response = None
+    auth_header = request.headers.get('Authorization')
+
+    if auth_header is not None:
+        access_token = auth_header.split(" ")[1]
+        decoded_token = ValidationHelper.decode_token(access_token)
+        if not isinstance(decoded_token, str):
+            if DatabaseManager().is_admin(decoded_token) is True:
+
+                candidate_reg_data = request.get_json(force=True)
+                sample_candidate = Candidates(oid, candidate_reg_data)
+
+                if len(candidate_reg_data) > 2:
+                    custom_response = jsonify(
+                        sample_candidate.more_data_fields_response), 400
+
+                elif len(candidate_reg_data) < 2:
+                    custom_response = jsonify(
+                        sample_candidate.few_data_fields_response), 400
+
+                elif sample_candidate.validate_candidate_reg_data() is None:
+                    custom_response = jsonify(
+                        sample_candidate.create_a_candidate()), 201
+
+                else:
+                    custom_response = \
+                        sample_candidate.validate_candidate_reg_data()
+            else:
+                custom_response = jsonify({
+                    "user": decoded_token,
+                    'message': "Restricted Access. Admin only"}), 401
+
+        else:
+            custom_response = jsonify({
+                "user": decoded_token,
+                'message': "Invalid Token"}), 401
+
+    else:
+        custom_response = jsonify(
+            {"status": 401, "message": "No Token Provided"}), 401
+
+    return custom_response
+
+
+@BASE_BP_V2.route("/office/open", methods=["GET"])
+def fetch_all_offices():
+    """ Fetch all political offices (GET) """
+
+    auth_header = request.headers.get('Authorization')
+    if auth_header is not None:
+        access_token = auth_header.split(" ")[1]
+        decoded_token = ValidationHelper.decode_token(access_token)
+
+        if not isinstance(decoded_token, str):
+            raw_candidates = \
+                DatabaseManager().fetch_all_records_in_a_table("candidates")
+
+            if raw_candidates == []:
+                custom_response = jsonify({
+                    "data": "The Candidate list is empty", "status": 200
+                }), 200
+            else:
+                data = []
+                for record in raw_candidates:
+                    sample = {}
+                    sample["Candidate ID"] = record["cid"]
+                    sample["User ID"] = record["uid"]
+                    sample["Office ID"] = record["oid"]
+                    sample["Party ID"] = record["pid"]
+                    sample["Registration Timestamp"] = \
+                        record["registration_timestamp"]
+                    data.append(sample)
+                custom_response = jsonify({
+                    "status": 200, "Registered Candidates": data
+                }), 200
+        else:
+            custom_response = jsonify({'message': decoded_token}), 401
+
+    else:
+        custom_response = jsonify(
+            {"status": 401, "message": "No Token Provided"}), 401
+
+    return custom_response
